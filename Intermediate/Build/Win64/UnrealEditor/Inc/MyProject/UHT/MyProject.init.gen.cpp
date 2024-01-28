63897e580d024ac3c1bd5f01ea40fa5d5410bb9e
alexeydr <55873949+alexeydr@users.noreply.github.com>
2024-01-28 10:14:50 +0400

    Строительство

--- Intermediate/Build/Win64/UnrealEditor/Inc/MyProject/UHT/MyProject.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/MyProject/UHT/MyProject.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeMyProject_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xAB04CE66,
-				0x80C3269C,
+				0x79BEFEFF,
+				0x3B345195,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MyProject.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_MyProject.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MyProject(Z_Construct_UPackage__Script_MyProject, TEXT("/Script/MyProject"), Z_Registration_Info_UPackage__Script_MyProject, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xAB04CE66, 0x80C3269C));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MyProject(Z_Construct_UPackage__Script_MyProject, TEXT("/Script/MyProject"), Z_Registration_Info_UPackage__Script_MyProject, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x79BEFEFF, 0x3B345195));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/MyProject/Public/BuildingSubsystem.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "BuildingSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UBuildingSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+
+
+
+};
